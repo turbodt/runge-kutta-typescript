7eab3415fecc51c7ae80df0d64c114ea6ca06187
Daniel T <daniel@localhost>
2025-06-19 16:11:46 +0200

    fix: Indentation

--- src/classes/runge-kutta.class.ts
+++ src/classes/runge-kutta.class.ts
@@ -1,87 +1,91 @@
 import ButcherTableau from "./butcher-tableaux.class";
 
+
 type _T = Float32Array | Float64Array | number[];
 const getNewT = <K extends _T>(x: K, ...args: any[]): K => {
-	if (x instanceof Float32Array) {
-		const newArgs = args as [any?, any?, any?];
-		return new Float32Array(...newArgs) as K;
-	} else if (x instanceof Float64Array) {
-		const newArgs = args as [any?, any?, any?];
-		return new Float64Array(...newArgs) as K;
-	} else if (x instanceof Array) {
-		return new Array(...args) as K;
-	}
-	return new Array() as K;
+    if (x instanceof Float32Array) {
+        const newArgs = args as [any?, any?, any?];
+        return new Float32Array(...newArgs) as K;
+    } else if (x instanceof Float64Array) {
+        const newArgs = args as [any?, any?, any?];
+        return new Float64Array(...newArgs) as K;
+    } else if (x instanceof Array) {
+        return new Array(...args) as K;
+    }
+    return new Array() as K;
 };
 
+
 const getNewCopyOfT = <K extends _T>(x: K): K => {
-	if (x instanceof Float32Array) {
-		return getNewT(x, x);
-	} else if (x instanceof Float64Array) {
-		return getNewT(x, x);
-	} else if (x instanceof Array) {
-		return x.slice() as K;
-	}
-	return new Array() as K;
+    if (x instanceof Float32Array) {
+        return getNewT(x, x);
+    } else if (x instanceof Float64Array) {
+        return getNewT(x, x);
+    } else if (x instanceof Array) {
+        return x.slice() as K;
+    }
+    return new Array() as K;
 };
+
 
 type F<T> = (t: number, x: T) => T;
 
+
 export default class RungeKutta<T extends _T> {
-	constructor(private butcherTableau: ButcherTableau, public f: F<T>) {}
+    constructor(private butcherTableau: ButcherTableau, public f: F<T>) {}
 
-	get order(): number {
-		return this.butcherTableau.order;
-	}
+    get order(): number {
+        return this.butcherTableau.order;
+    }
 
-	public k(i: number, h: number, t: number, x: T, k: T[]): T {
-		const xk = getNewCopyOfT(x);
-		for (let j = 0; j < i; j++) {
-			for (let l = 0; l < xk.length; l++) {
-				xk[l] += h * this.butcherTableau.a[i][j] * k[j][l];
-			}
-		}
-		return this.f(t + h * this.butcherTableau.c[i], xk);
-	}
+    public k(i: number, h: number, t: number, x: T, k: T[]): T {
+        const xk = getNewCopyOfT(x);
+        for (let j = 0; j < i; j++) {
+            for (let l = 0; l < xk.length; l++) {
+                xk[l] += h * this.butcherTableau.a[i][j] * k[j][l];
+            }
+        }
+        return this.f(t + h * this.butcherTableau.c[i], xk);
+    }
 
-	public stepInto(h: number, t: number, x: T, xNew: T): RungeKutta<T> {
-		const k: T[] = [];
-		for (let i = 0; i < this.order; i++) {
-			k.push(this.k(i, h, t, x, k));
-		}
-		for (let j = 0; j < this.order; j++) {
-			for (let l = 0; l < x.length; l++) {
-				xNew[l] = h * this.butcherTableau.b[j] * k[j][l] + x[l];
-			}
-		}
-		return this;
-	}
+    public stepInto(h: number, t: number, x: T, xNew: T): RungeKutta<T> {
+        const k: T[] = [];
+        for (let i = 0; i < this.order; i++) {
+            k.push(this.k(i, h, t, x, k));
+        }
+        for (let j = 0; j < this.order; j++) {
+            for (let l = 0; l < x.length; l++) {
+                xNew[l] = h * this.butcherTableau.b[j] * k[j][l] + x[l];
+            }
+        }
+        return this;
+    }
 
-	public step(h: number, t: number, x: T): T {
-		const xNew = getNewT(x, x.length);
-		this.stepInto(h, t, x, xNew);
-		return xNew;
-	}
+    public step(h: number, t: number, x: T): T {
+        const xNew = getNewT(x, x.length);
+        this.stepInto(h, t, x, xNew);
+        return xNew;
+    }
 
-	public stepsInto(
-		n: number,
-		h: number,
-		t: number,
-		x: T,
-		xNew: T
-	): RungeKutta<T> {
-		for (let l = 0; l < x.length; l++) {
-			xNew[l] = x[l];
-		}
-		for (let i = 0; i < n; i++) {
-			this.stepInto(h, t, xNew, xNew);
-		}
-		return this;
-	}
+    public stepsInto(
+        n: number,
+        h: number,
+        t: number,
+        x: T,
+        xNew: T
+    ): RungeKutta<T> {
+        for (let l = 0; l < x.length; l++) {
+            xNew[l] = x[l];
+        }
+        for (let i = 0; i < n; i++) {
+            this.stepInto(h, t, xNew, xNew);
+        }
+        return this;
+    }
 
-	public steps(n: number, h: number, t: number, x: T): T {
-		const xNew = getNewCopyOfT(x);
-		this.stepsInto(n, h, t, xNew, xNew);
-		return xNew;
-	}
+    public steps(n: number, h: number, t: number, x: T): T {
+        const xNew = getNewCopyOfT(x);
+        this.stepsInto(n, h, t, xNew, xNew);
+        return xNew;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export {
-	default as ButcherTableau,
-	rk4ButcherTableau
+    default as ButcherTableau,
+    rk4ButcherTableau
 } from "./classes/butcher-tableaux.class";
 export { default as RungeKutta } from "./classes/runge-kutta.class";
